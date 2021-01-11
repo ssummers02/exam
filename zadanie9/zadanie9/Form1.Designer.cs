24f215f2c2d8eb7f6719dfba3a7e264f102091e9
scumethy <62615794+scumethy@users.noreply.github.com>
2021-01-11 20:42:31 +0300

    slauobtmatr
    slauobtmatr

---
+++ zadanie9/zadanie9/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace zadanie9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(107, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(141, 78);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Матрица коэффициентов СЛАУ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Свободные члены";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Location = new System.Drawing.Point(351, 115);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(62, 105);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(172, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x1 = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(172, 416);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "x2 = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 370);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(229, 418);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 571);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ zadanie9/zadanie9/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie9
+{
+    public partial class Form1 : Form
+    {
+        int[,] origMatrix = new int[2, 2];
+        int[,] minorMatrix = new int[2, 2];
+        int[] freeMemMatrix = new int[2];
+        double[,] inverseMatrix = new double[2, 2];
+        double[] resultMatrix = new double[2];
+        int determinant;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = 2;
+            dataGridView1.ColumnCount = 2;
+            dataGridView2.RowCount = 2;
+            dataGridView2.ColumnCount = 1;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    dataGridView1[i, j] = new DataGridViewTextBoxCell();
+                }
+            }
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.Width = 40;
+            }
+            foreach (DataGridViewColumn col in dataGridView2.Columns)
+            {
+                col.Width = 40;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // input original matrix
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        origMatrix[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                }
+                // input free members matrix
+                for (int i = 0; i < 2; i++)
+                {
+                    freeMemMatrix[i] = Convert.ToInt32(dataGridView1.Rows[1].Cells[i].Value);
+                }
+                
+                determinant = origMatrix[0, 0] * origMatrix[1, 1] - origMatrix[0, 1] * origMatrix[1, 0];
+                if (Math.Abs(determinant) == 0) throw new Exception("Обратной матрицы не существует!");
+                
+                minorMatrix = cosntructMinorMatrix(origMatrix);
+                
+                //double div_det = (-1.0 / determinant);
+                //inverseMatrix[0, 0] = minorMatrix[0, 0] * div_det;
+                //inverseMatrix[0, 1] = minorMatrix[0, 1] * div_det;
+                //inverseMatrix[1, 0] = minorMatrix[1, 0] * div_det;
+                //inverseMatrix[1, 1] = minorMatrix[1, 1] * div_det;
+
+                resultMatrix[0] = minorMatrix[0, 0] * freeMemMatrix[0] + minorMatrix[0, 1] * freeMemMatrix[1];
+                resultMatrix[1] = minorMatrix[1, 0] * freeMemMatrix[0] + minorMatrix[1, 1] * freeMemMatrix[1];
+                for (int j = 0; j < 2; j++)
+                {
+                    MessageBox.Show(resultMatrix[j].ToString());
+                }
+                double div_det = (-1.0 / determinant);
+                resultMatrix[0] = resultMatrix[0] * div_det;
+                resultMatrix[1] = resultMatrix[1] * div_det;
+
+                textBox1.Text = resultMatrix[0].ToString();
+                textBox2.Text = resultMatrix[1].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public int[,] cosntructMinorMatrix(int[,] matrix)
+        {
+            int[,] resMatrix = new int[2, 2];
+            resMatrix[0, 0] = matrix[1,1];
+            resMatrix[0, 1] = matrix[0, 1] * -1;
+            resMatrix[1, 0] = matrix[1, 0] * -1;
+            resMatrix[1, 1] = matrix[0, 0];
+
+            return resMatrix;
+        }
+
+        public double calculateDeterminant(int[,] matrix)
+        {
+            double det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            return det;
+        }
+    }
+}
